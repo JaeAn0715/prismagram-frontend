34ab6418fc139387b0bda7085bba566d2744ff73
An Jaemyeong <jae@Anui-MacBookPro.local>
2020-08-28 11:54:42 +0900

    live server

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -2,7 +2,10 @@ import ApolloClient from "apollo-boost";
 import { resolvers, defaults } from "./LocalState";
 
 export default new ApolloClient({
-  uri: "http://localhost:4000",
+  uri:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:4000"
+      : "https://jstargram-2.herokuapp.com/",
   clientState: {
     defaults: defaults,
     resolvers,
